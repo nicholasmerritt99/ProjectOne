7d77f973e5abb03d0c5ffaa088fdb12cfe2a0e48
Nicholas Merritt <nicholasmerritt1994@gmail.com>
2019-04-24 13:20:46 -0500

    added to my Java for my API

--- logic1.js
+++ logic1.js
@@ -1,35 +1,18 @@
-button.onclick = function() {
-    var startPos;
-    var nudge = document.getElementById("nudge");
-  
-    var showNudgeBanner = function() {
-      nudge.style.display = "block";
-    };
-  
-    var hideNudgeBanner = function() {
-      nudge.style.display = "none";
-    };
-  
-    var nudgeTimeoutId = setTimeout(showNudgeBanner, 5000);
-  
-    var geoSuccess = function(position) {
-      hideNudgeBanner();
-      // We have the location, don't display banner
-      clearTimeout(nudgeTimeoutId);
-  
-      // Do magic with location
-      startPos = position;
-      document.getElementById('startLat').innerHTML = startPos.coords.latitude;
-      document.getElementById('startLon').innerHTML = startPos.coords.longitude;
-    };
-    var geoError = function(error) {
-      switch(error.code) {
-        case error.TIMEOUT:
-          // The user didn't accept the callout
-          showNudgeBanner();
-          break;
-      }
-    };
-  
-    navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
-  };
+//<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    //<script type="text/javascript">
+    
+      $("#find-movie").on("click", function(event) {
+        event.preventDefault();
+        var movie = $("#movie-input").val();
+        var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+
+        $.ajax({
+      url: queryURL,
+      method: "GET"
+      }).then(function(response) {
+
+        $('#movie-view').text(JSON.stringify(response));
+        console.log(response)
+
+      });
+    });

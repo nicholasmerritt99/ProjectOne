ecdb9d50919a7512d9c6306859fba241e73944ff
Nicholas Merritt <nicholasmerritt1994@gmail.com>
2019-04-24 19:57:59 -0500

    added ID's and added java

---
+++ logic2.js
@@ -0,0 +1,15 @@
+$("#findMovie").on("click", function(event) {
+    event.preventDefault();
+    var movie = $("#movieInput").val();
+    var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+
+    $.ajax({
+  url: queryURL,
+  method: "GET"
+  }).then(function(response) {
+
+    //$('#movie-view').text(JSON.stringify(response));
+    console.log(response)
+
+  });
+});

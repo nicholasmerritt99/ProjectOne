5d7650f58402fa11c40111cbf79b283b6ef6a62c
Nicholas Merritt <nicholasmerritt1994@gmail.com>
2019-04-24 20:27:11 -0500

    added to my java

--- logic1.js
+++ logic1.js
@@ -1,35 +1,35 @@
-button.onclick = function() {
-  var startPos;
-  var nudge = document.getElementById("nudge");
+// button.onclick = function() {
+//   var startPos;
+//   var nudge = document.getElementById("nudge");
 
-  var showNudgeBanner = function() {
-    nudge.style.display = "block";
-  };
+//   var showNudgeBanner = function() {
+//     nudge.style.display = "block";
+//   };
 
-  var hideNudgeBanner = function() {
-    nudge.style.display = "none";
-  };
+//   var hideNudgeBanner = function() {
+//     nudge.style.display = "none";
+//   };
 
-  var nudgeTimeoutId = setTimeout(showNudgeBanner, 5000);
+//   var nudgeTimeoutId = setTimeout(showNudgeBanner, 5000);
 
-  var geoSuccess = function(position) {
-    hideNudgeBanner();
-    // We have the location, don't display banner
-    clearTimeout(nudgeTimeoutId);
+//   var geoSuccess = function(position) {
+//     hideNudgeBanner();
+//     // We have the location, don't display banner
+//     clearTimeout(nudgeTimeoutId);
 
-    // Do magic with location
-    startPos = position;
-    document.getElementById('startLat').innerHTML = startPos.coords.latitude;
-    document.getElementById('startLon').innerHTML = startPos.coords.longitude;
-  };
-  var geoError = function(error) {
-    switch(error.code) {
-      case error.TIMEOUT:
-        // The user didn't accept the callout
-        showNudgeBanner();
-        break;
-    }
-  };
+//     // Do magic with location
+//     startPos = position;
+//     document.getElementById('startLat').innerHTML = startPos.coords.latitude;
+//     document.getElementById('startLon').innerHTML = startPos.coords.longitude;
+//   };
+//   var geoError = function(error) {
+//     switch(error.code) {
+//       case error.TIMEOUT:
+//         // The user didn't accept the callout
+//         showNudgeBanner();
+//         break;
+//     }
+//   };
 
-  navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
-};
+//   navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+// };
